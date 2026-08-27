9d9810ecfd8376151ab5aca7e1d4a03fc69a10e2
Dario Neuenschwander <110724880+Vadergit@users.noreply.github.com>
2026-08-27 18:16:56 +0200

    Add Cube theme-data compatibility shim

---
+++ cube/themes-data.js
@@ -0,0 +1,3 @@
+/* Compatibility shim for cached versions of ../themes.js.
+   Older cached loaders resolve themes-data.js relative to /cube/. */
+document.write('<script src="../themes-data.js?v=1.4.4"><\/script>');
