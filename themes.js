87aa58063ce27606095691fc6be56af2121e2b18
Dario Neuenschwander <110724880+Vadergit@users.noreply.github.com>
2026-09-01 11:09:49 +0200

    Load shared variety logic in all game modes

--- themes.js
+++ themes.js
@@ -8,6 +8,7 @@
   const script=document.currentScript;
   const base=script?new URL('.',script.src):new URL('./',location.href);
   const dataUrl=new URL('themes-data.js?v=1.4.4',base).href;
+  const varietyUrl=new URL('variety-hotfix.js?v=1.5.0',base).href;
   const isCube=/\/cube(?:\/|$)/i.test(location.pathname);
 
   let html=`<script src="${dataUrl}"><\/script>`;
@@ -15,5 +16,6 @@
     const gridUrl=new URL('grid-7-8-hotfix.js?v=1.4.3',base).href;
     html+=`<script src="${gridUrl}"><\/script>`;
   }
+  html+=`<script src="${varietyUrl}"><\/script>`;
   document.write(html);
 })();
